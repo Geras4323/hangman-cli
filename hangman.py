dc4b103e1c2efc12b96009b3111c5ba197e80f82
Geras4323 <german432yo@gmail.com>
2022-04-03 19:14:30 -0300

    Correccion UTF-8

--- hangman.py
+++ hangman.py
@@ -1,10 +1,9 @@
 def file_opening():
-    file = open("./archivos/data.txt", "r")
+    file = open("./archivos/data.txt", "r", encoding="utf-8")
     return file
 
 def run():
     f = file_opening()
-
 
 if __name__ == '__main__':
     run()
