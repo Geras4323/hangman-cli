458ac77179833787c9835814f4796eb6bf15b73d
Geras4323 <german432yo@gmail.com>
2022-04-03 19:45:34 -0300

    Agregado el codigo para elegir una palabra al azar

--- hangman.py
+++ hangman.py
@@ -1,3 +1,6 @@
+import random
+
+
 def file_opening():
     file = open("./archivos/data.txt", "r", encoding="utf-8")
     return file
@@ -8,9 +11,15 @@ def word_loading(file):
         word_list.append(str(word))
     return word_list
 
+def word_picking(word_list):
+    rand_word = random.choice(word_list)    # agarra una palabra de la lista al azar
+    return rand_word
+
+
 def run():
     f = file_opening()          # abrimos el archivo data.txt
     words = word_loading(f)     # cargamos las palabras en una lista
+    chosen_word = word_picking(words)
 
 
 if __name__ == '__main__':
