f7ffea839be2be12e51dd7377d5aeaefb2e8632e
Geras4323 <german432yo@gmail.com>
2022-04-03 19:27:28 -0300

    Agregado el codigo para cargar las palabras desde data.txt

--- hangman.py
+++ hangman.py
@@ -2,8 +2,16 @@ def file_opening():
     file = open("./archivos/data.txt", "r", encoding="utf-8")
     return file
 
+def word_loading(file):
+    word_list = []
+    for word in file:
+        word_list.append(str(word))
+    return word_list
+
 def run():
     f = file_opening()
+    words = word_loading(f)
+
 
 if __name__ == '__main__':
     run()
